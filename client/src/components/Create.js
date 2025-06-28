dcee2180a3573f534d7056154139ef0747448cbb
Ryan Vaughn <rqv5203@psu.edu>
2025-06-27 20:10:38 -0400

    format consistency (#5)
    Co-authored-by: ryanvaughn01 <rcvaughn01@gmail.com>

--- client/src/components/Create.js
+++ client/src/components/Create.js
@@ -128,7 +128,9 @@ const Create = ({ user }) => {
   };
 
   const formatDate = (dateString) => {
-    return new Date(dateString).toLocaleDateString('en-US', {
+    const [year, month, day] = dateString.split('-');
+    const date = new Date(year, month - 1, day);
+    return date.toLocaleDateString('en-US', {
       weekday: 'short',
       month: 'short',
       day: 'numeric'
